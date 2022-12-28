d56ea949bb6d07ce810670f040c4b39d637c9f79
Dexametasona <machicadogomezalexander@gmail.com>
2022-12-28 15:30:56 -0500

    reinicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BodyModule } from './body/body.module';
-import { FooterModule } from './footer/footer.module';
-import { HeaderModule } from './header/header.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, BodyModule, FooterModule, HeaderModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
